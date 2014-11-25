bc947015453651fd3c491ec5ae079996ee28fe3d
Henning Dickten <hdickten@uni-bonn.de>
2014-11-25 09:34:52 +0100

    ipython-extension: added/fixed memory_profiler

--- .ipython/extensions/memory_profiler_ext.py
+++ .ipython/extensions/memory_profiler_ext.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python
 # coding: utf-8
 """
-    add %magic for line_profiler extension
+    add %magic for memory_profiler extension
 """
 __author__ = 'Henning Dickten'
 
 try:
-    import line_profiler
+    import memory_profiler
 except ImportError:
     pass
 
 
 def load_ipython_extension(ip):
-    ip.define_magic('lprun', line_profiler.magic_lprun)
+    ip.define_magic('memit', memory_profiler.magic_memit)
+    ip.define_magic('mprun', memory_profiler.magic_mprun)
 
 
 if __name__ == '__main__':
