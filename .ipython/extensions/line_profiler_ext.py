d6d8517b89422065abd0bd807a3030f86eff4ac1
Henning Dickten <hdickten@uni-bonn.de>
2014-11-25 09:24:53 +0100

    ipython: initial config with line_profiler

---
+++ .ipython/extensions/line_profiler_ext.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# coding: utf-8
+"""
+    add %magic for line_profiler extension
+"""
+__author__ = 'Henning Dickten'
+
+try:
+    import line_profiler
+except ImportError:
+    pass
+
+
+def load_ipython_extension(ip):
+    ip.define_magic('lprun', line_profiler.magic_lprun)
+
+
+if __name__ == '__main__':
+    pass
